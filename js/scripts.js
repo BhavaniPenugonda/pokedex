0f0b517a0a9ce3d9a7c37c02245beeaac1803e1c
BhavaniPenugonda <bhavani0202@gmail.com>
2024-07-11 10:56:12 +0200

    scripts.js
    Removed the entire code from page. Added variable ArrayList of Pokemon that contains 3 objects.Each object has name, height and type

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-const favouriteFood= 'pizza';
-document.write(favouriteFood);
+const pokemonList = [
+  {name :'Pikachu', height:0.4, type : ['electric']},
+  {name :'Pidgeot', height:1.5, type : ['Flying','Normal']},
+  {name :'Caterpie', height:0.3, type : ['Bug']}
+];
