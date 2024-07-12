695ec911bdb642bf587fc85ef467c88d5bd8de86
BhavaniPenugonda <bhavani0202@gmail.com>
2024-07-12 22:23:46 +0200

    scripts.js
    Added comments and changed the var declaration to let.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
-const pokemonList = [
+
+//pokemon list in array //
+let pokemonList = [
   {name :'Pikachu', height:0.4, type : ['electric']},
   {name :'Pidgeot', height:1.5, type : ['Flying','Normal']},
   {name :'Caterpie', height:0.3, type : ['Bug']}
 ];
+
+//conditional loop that prints the Pokemon list and tells which is the big pokemon//
+for(let i=0;i<pokemonList.length;i++) {
+   document.write('<p>'+  pokemonList[[i]].name  + '(height: ' +  pokemonList[i].height +  ')');
+   if(pokemonList[i].height>1){
+    document.write('-Wow,that\'s big');
+   }
+
+}
