9cee5e5945ffbf315b83e2856d05b6a924bb508c
BhavaniPenugonda <bhavani0202@gmail.com>
2024-07-19 10:50:37 +0200

    scripts.js
    getAll function is replaced before add function

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 
-// pokemon list in array //
 let pokemonRepository= (function(){
+
+
   let pokemonList = [
     {name :'Pikachu', height:0.4, type : ['electric']},
     {name :'Pidgeot', height:1.5, type : ['Flying','Normal']},
     {name :'Caterpie', height:0.3, type : ['Bug']}
   ];
-  
-  function add(pokemon){
-    pokemonList.push(pokemon);
-  }
-  
+
+
   function getAll(){
     return pokemonList;
+  }
+
+  function add(pokemon){
+    pokemonList.push(pokemon);
   }
   
   return{
@@ -23,9 +25,8 @@ let pokemonRepository= (function(){
 })();
 
 
-
 pokemonList.forEach(function(pokemon) {
-  document.write(pokemon.name   +  '(height: ' +  pokemon.height +  ')');
+  document.write('<p>' +pokemon.name  +  '(height: ' +  pokemon.height +  ')');
   if(pokemon.height>1){
     document.write('-Wow,that\'s big!');
   }
