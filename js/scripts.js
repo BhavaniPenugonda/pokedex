0befaf4f0cb2ef7c079a3e675592acfa8489847d
BhavaniPenugonda <bhavani0202@gmail.com>
2024-07-22 23:40:18 +0200

    scripts.js
    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,14 @@
 
 const pokemonRepository= (function(){
 
-
+/* pokemon Array list  is created and assigned to a variable */
   let pokemonList = [
     {name :'Pikachu', height:0.4, type : ['electric']},
     {name :'Pidgeot', height:1.5, type : ['Flying','Normal']},
     {name :'Caterpie', height:0.3, type : ['Bug']}
   ];
 
-
+/*Get all pokemons*/
   function getAll(){
     return pokemonList;
   }
@@ -16,7 +16,7 @@ const pokemonRepository= (function(){
   function add(pokemon){
     pokemonList.push(pokemon);
   }
-
+/* prints the pokemon name, when pokemon button is clicked */
   function addListItem(pokemon){
     let pokemonList = document.querySelector('.pokemon-list');
     let listpokemon = document.createElement('li');
@@ -45,7 +45,7 @@ const pokemonRepository= (function(){
   };
 })();
 
-
+/* loop  that prints all pokemons with eventlistener   */
 pokemonRepository.getAll().forEach(function(pokemon) {
   pokemonRepository.addListItem(pokemon);
   
